b399bb2a80ede76de6826f0219aa08eb19d2a34a
PengYiLin <479234318@qq.com>
2017-05-10 10:29:34 +0100

    email model

--- maxi/resources/views/EmailModule.blade.php
+++ maxi/resources/views/EmailModule.blade.php
@@ -2,13 +2,11 @@
 <html>
 <head>
 
-	<meta charset="utf-8"> 
-	<title>Email Module</title>
+  <meta charset="utf-8"> 
+  <title>Email Module</title>
   <script src="{{ URL::asset('/js/jquery.js') }}"></script>
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/shiyishi.css') }}">
-
-
 
   <script>
    $(function() {
@@ -19,65 +17,64 @@
   });
 </script>
 
-
 </head>
 
 <body>
-	
- <div id="header" style="background-color:#fff;"><br>
 
-  @if (Auth::guest())
+  <div id="header" style="background-color:#fff;"><br>
 
-  <div class="text-right links font">
-    <a href="{{ route('login') }}">Login</a>
-    <a href="{{ route('register') }}">Register</a>
+    @if (Auth::guest())
+
+    <div class="text-right links font">
+      <a href="{{ route('login') }}">Login</a>
+      <a href="{{ route('register') }}">Register</a>
+    </div>
+
+    @else
+
+    <div class="text-right links font">
+      <a> 
+        <a> User: </a>
+        {{ Auth::user()->name }}
+      </a>
+      <a href="{{ route('logout') }}"
+      onclick="event.preventDefault();
+      document.getElementById('logout-form').submit();">Logout</a>
+      <a href="{{ url('/main/'.Auth::user()->name) }}">Main</a>
+
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+
+        {{ csrf_field() }}
+
+      </form>
+    </div>
+
+    @endif
   </div>
 
-  @else
-
-  <div class="text-right links font">
-    <a> 
-      <a> User: </a>
-      {{ Auth::user()->name }}
-    </a>
-    <a href="{{ route('logout') }}"
-    onclick="event.preventDefault();
-    document.getElementById('logout-form').submit();">Logout</a>
-    <a href="{{ url('/main/'.Auth::user()->name) }}">Main</a>
-
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-
-      {{ csrf_field() }}
-
-    </form>
+  <div id=header class="header">
+    <div class="col-md-4">
+     <a href="{{ url('/home') }}"><img src="{{URL::asset('/images/TUOS_Logo_CMYK.png')}}" alt="profile Pic" height="200" style="margin-left: 13%;" ></a>
+   </div>
+   <div class="col-md-8">
+     <div class="text-center" style="font-size: 50px; margin-top:5%">
+      <h>Overseas Student Recruitment Management</h>
+    </div>
   </div>
-
-  @endif
-</div>
-
-<div id=header class="header">
-  <div class="col-md-4">
-   <a href="{{ url('/home') }}"><img src="{{URL::asset('/images/TUOS_Logo_CMYK.png')}}" alt="profile Pic" height="200" style="margin-left: 13%;" ></a>
- </div>
- <div class="col-md-8">
-   <div class="text-center" style="font-size: 50px; margin-top:5%">
-    <h>Overseas Student Recruitment Management</h>
-  </div>
-</div>
 </div>
 <div class="col-md-12">
-    <div class="text-right" style="background-color: #66B3FF">
-        <a class="btn btn-link" style="color:white" href="{{ url('/course') }}" role="button">Add Courses</a>
-        <a class="btn btn-link" style="color:white" href="{{ url('/department') }}" role="button">Add Department</a>
-        <a href="{{ url('/upload') }}"><button type="button" style="color:white" class="btn btn-link"> Upload File</button></a>
-        <a href="{{ url('/emailmodule') }}"><button type="button" style="color:white" class="btn btn-link"  >Email Modification</button></a>
-        <button type="button" style="color:white" class="btn btn-link" onclick="feedBack()">Feedback</button>
-    </div>
+  <div class="text-right" style="background-color: #66B3FF">
+    <a href="{{ url('/upload') }}"><button type="button" style="color:white" class="btn btn-link"> Upload File</button></a>
+    <a class="btn btn-link" style="color:white" href="{{ url('/course') }}" role="button">Add Courses</a>
+    <a class="btn btn-link" style="color:white" href="{{ url('/department') }}" role="button">Add Department</a>
+    <a href="{{ url('/emailmodule') }}"><button type="button" style="color:white" class="btn btn-link"  >Email Modification</button></a>
+    <button type="button" style="color:white" class="btn btn-link" onclick="feedBack()">Feedback</button>
+  </div>
 </div>
 
 <div class="text-center col-md-12 content">
-	<br>
- <span style="font-size:45px;margin-left: 5%">Email Module</span>
+  <br>
+  <span style="font-size:45px;margin-left: 5%">Email Module</span>
 </div>
 
 
@@ -87,10 +84,9 @@
   <div class="col-md-9" style="margin-left: 15%; border: 4px solid #a1a1a1;margin-top: 20px;padding: 50px;">
 
     <div class="form-group">
-      <table class="table table-hover">
+      <table >
         <tr>
-          <th>Content Name</th>
-          <th></th>
+          <th>Content Name</th> 
         </tr>
         <tr>
           <td> 
@@ -104,58 +100,58 @@
 
            </select>
          </td>
+       </tr>
+
+     </table><br>
+
+
+     <form method="POST" enctype="multipart/form-data" action="{{url('emailmodule')}}">
+       {{ csrf_field() }}
+
+       <label>Name</label>
+       <input class="form-control" name="name" value="{{ $emailModule['name']}}"><br>
+
+       <label>Content Type</label>
+       <input class="form-control" value="{{$module['type']}}"><br>
+
+       <label>Content</label>
+       <textarea name="content" rows="6"  class="form-control" > {{$emailModule['content']}} </textarea>
+       <br>
+
+       <label>Manager</label>
+       @if(($emailModule['manager'])!="")
+       <input class="form-control" name="manager" value="{{ $emailModule['manager'] }}" readonly>
+       @else
+       <input class="form-control" name="manager" value=" {{ Auth::user()->name }}" readonly>
+       @endif
+
+       <br>
+       <table style="margin-left: 30%">
+        <tr>
+
          <td>
-          <button class="btn btn-link" type="button" style="color:white" ><a id="a_id">Search</a></button>
-        </td>
+          <input name="manager" value="{{ Auth::user()->name }}" hidden="">
+          <button class="btn btn-primary " name="id" value="{{ $emailModule['id']}}" type="submit" style="color:white;float:left;margin-left:40px;" >Update</button></td>
+
+        </form>
+
+        <form method="POST" enctype="multipart/form-data" action="{{url('emailmodule/delete')}}">
+
+         <td>
+          <input name="manager" value="{{ Auth::user()->name }}" hidden="">
+          <button class="btn btn-primary " name="id" type="submit" value="{{ $emailModule['id']}}" style="color:white;float:left;margin-left:40px;" >Delete</button></td>
+        </form>
+        <td><a href="{{ url('/main/'.Auth::user()->name) }}"><button class="btn btn-primary " type="button" style="color:white;float:left;margin-left:40px;" > Cancel</button></a></td>
       </tr>
-
     </table>
 
 
-    <form method="POST" enctype="multipart/form-data" action="{{url('emailmodule')}}">
-     {{ csrf_field() }}
-
-     <label>Name</label>
-   </label><input class="form-control" name="name" value="{{ $emailModule['name']}}"><br/>
-
-   <label>Content</label>
-   <textarea name="content" rows="6"  class="form-control" > {{$emailModule['content']}} </textarea>
-   <br>
-
-   <label>Manager</label>
-   @if(($emailModule['manager'])!="")
-   <input class="form-control" name="manager" value="{{ $emailModule['manager'] }}" readonly>
-   @else
-   <input class="form-control" name="manager" value=" {{ Auth::user()->name }}" readonly>
-   @endif
-
-    <br>
-    <table style="margin-left: 30%">
-    <tr>
-    
-     <td>
-      <input name="manager" value="{{ Auth::user()->name }}" hidden="">
-      <button class="btn btn-primary " name="id" value="{{ $emailModule['id']}}" type="submit" style="color:white;float:left;margin-left:40px;" >Update</button></td>
-
-   </form>
-
-   <form method="POST" enctype="multipart/form-data" action="{{url('emailmodule/delete')}}">
-
-     <td>
-      <input name="manager" value="{{ Auth::user()->name }}" hidden="">
-      <button class="btn btn-primary " name="id" type="submit" value="{{ $emailModule['id']}}" style="color:white;float:left;margin-left:40px;" >Delete</button></td>
-   </form>
-    <td><a href="{{ url('/main/'.Auth::user()->name) }}"><button class="btn btn-warning " type="button" style="color:white;float:left;margin-left:40px;" > Cancel</button></a></td>
-    </tr>
-    </table>
-
-
-</div>
+  </div>
 </div>
 
 </body>
 <footer>
-	<div class="text-center col-md-12 content" >
+  <div class="text-center col-md-12 content" >
     <p>University of Sheffield/ Computer of Science/ Software System of Internet Technology/ Maxi Project/ Team Three </p>  
   </div>
 </footer>
